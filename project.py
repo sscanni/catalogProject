e302878b0de1e1c647e5361c90f60a0cb08e5880
sscanni <steven.scanniello@snhu.edu>
2018-11-13 13:11:12 -0500

    Corrrect code that displays page headers. Move old html files to another subfolder

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker, relationship, joinedload
